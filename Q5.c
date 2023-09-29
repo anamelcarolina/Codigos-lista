86a12414e56d9aa48155701e1c48cf873161c083
anamelcarolina <129069189+anamelcarolina@users.noreply.github.com>
2023-09-28 22:27:54 -0300

    Update Q5.c

--- Q5.c
+++ Q5.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 
-int main(){
-
+int main() {
     long a, b;
-    printf("Insira um valor positivo entre 0 e 1.000.000.000: ");
+    printf("Digite um valor positivo entre 0 e 1.000.000.000: ");
     scanf("%ld", &a);
-    printf("Insira outro valor positivo entre 0 e 1.000.000.000: ");
+    printf("Digite outro valor positivo entre 0 e 1.000.000.000: ");
     scanf("%ld", &b);
     
     if (a == b) {
-        printf("Os valores lidos sao iguais\n");
-    } else {
-        if (a > b) {
-            printf("%ld eh maior que %ld\n", a, b);
-            if (a%b == 0)
-                printf("%ld eh multiplo de %ld\n", a, b);
-            else
-                printf("%ld  nao eh multiplo de %ld\n", a, b);
+        printf("Os valores lidos são iguais\n");
+    } else if (a > b) {
+        printf("%ld é maior que %ld\n", a, b);
+        if (a % b == 0) {
+            printf("%ld é um múltiplo de %ld\n", a, b);
         } else {
-            if (b > a){
-                printf("%ld eh maior que %ld\n", b, a);
-                if (b%a == 0)
-                    printf("%ld eh multiplo de %ld\n", b, a);
-                else
-                    printf("%ld  nao eh multiplo de %ld\n", b, a);
-            }
+            printf("%ld não é um múltiplo de %ld\n", a, b);
+        }
+    } else {
+        printf("%ld é maior que %ld\n", b, a);
+        if (b % a == 0) {
+            printf("%ld é um múltiplo de %ld\n", b, a);
+        } else {
+            printf("%ld não é um múltiplo de %ld\n", b, a);
         }
     }
     return 0;
