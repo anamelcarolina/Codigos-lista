fa598863c0b854def2029ab42ae59e3fd45d22f0
anamelcarolina <129069189+anamelcarolina@users.noreply.github.com>
2023-09-28 22:24:27 -0300

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -4,12 +4,12 @@ int main(){
 
     long valor, constValor;
     long soma = 0;
-    printf("Insira um valor positivo entre 0 e 1.000.000.000: ");
+    
     scanf("%ld", &valor);
     constValor = valor;
     if (valor == 0){
         printf("%ld eh par\n", valor);
-        printf("A soma dos algarismos de %ld eh igual a %ld\n", valor, valor);
+        printf("A soma dos algarismos de %ld eh %ld\n", valor, valor);
     } else {
         if (valor%2 == 0){
             printf("%ld eh par\n", valor);
